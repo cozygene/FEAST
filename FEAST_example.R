e16810f7d189d46f890430d4bbe4dc7f7866f094
liashenhav <35780866+liashenhav@users.noreply.github.com>
2019-06-10 09:16:58 -0700

    Update FEAST_example.R

--- FEAST_example.R
+++ FEAST_example.R
@@ -1,18 +1,24 @@
 rm(list = ls())
 gc()
 
-print("Change dir_path")
-dir_path = paste("~/Dropbox/FEAST-master/")
-setwd(paste0(dir_path, "code"))
+print("Change directory path")
+dir_path = paste("~/FEAST")
+setwd(paste0(dir_path, "FEAST_src"))
 source("src.R")
+
+#set the arguments of you data
+metadata_file = "metadata_example.txt" #metadata file name
+count_matrix = "otu_example.txt" #count_matrix file name
+num_sources <- 7 #number of sources
+EM_iterations = 1000 # number of EM iterations. default value
 
 setwd(paste0(dir_path, "Data_files"))
 
 # Load sample metadata
-metadata <- read.csv('metadata_example.txt',h=T, sep = "\t", row.names = 1)
+metadata <- read.csv(file = metadata_file ,header = T, sep = "\t", row.names = 1)
 
 # Load OTU table
-otus <- read.table('otu_example.txt', header = T, comment = '', check = F, sep = '\t')
+otus <- read.table(file = count_matrix, header = T, comment = '', check = F, sep = '\t')
 otus <- t(as.matrix(otus))
 
 
@@ -30,54 +36,31 @@ if(length(common.sample.ids) <= 1) {
 
 
 
-EM_iterations = 100
+
 envs <- metadata$Env
-Ids <- unique(metadata$id)
-Proportions_est <- list()
-num_sources <- 3
 
-for(it in 1:length(Ids)){
-  
+# Extract the source environments and source/sink indices
 
-  # Extract the source environments and source/sink indices
-  
-  train.ix <- which(metadata$SourceSink=='Source' & metadata$id == Ids[it])
-  test.ix <- which(metadata$SourceSink=='Sink' & metadata$id == Ids[it])
-  COVERAGE =  min(rowSums(otus[c(train.ix, test.ix),]))  #Can be adjusted by the user
-  
-  # Define sources and sinks
-  
-  sources <- as.matrix(rarefy(otus[train.ix,], COVERAGE))
-  sinks <- as.matrix(rarefy(t(as.matrix(otus[test.ix,])), COVERAGE))
-  
-  
-  print(paste("Number of OTUs in the sink sample = ",length(which(sinks > 0))))
-  print(paste("Seq depth in the sources and sink samples = ",COVERAGE))
-  print(paste("The sink is:", envs[test.ix]))
-  
-  # Estimate source proportions for each sink
-  
-  FEAST_output<-FEAST(source=sources, sinks = t(sinks), env = envs[train.ix], em_itr = EM_iterations, COVERAGE = COVERAGE)
-  Proportions_est[[it]] <- FEAST_output$data_prop[,1]
-  
-  
-  names(Proportions_est[[it]]) <- c(as.character(envs[train.ix]), "unknown")
-  
-  if(length(Proportions_est[[it]]) < num_sources +1){
+train.ix <- which(metadata$SourceSink=='Source')
+test.ix <- which(metadata$SourceSink=='Sink')
+COVERAGE =  min(rowSums(otus[c(train.ix, test.ix),]))  #Can be adjusted by the user
 
-    tmp = Proportions_est[[it]]
-    Proportions_est[[it]][num_sources] = NA
-    Proportions_est[[it]][num_sources+1] = tmp[num_sources]
-  }
-  
-  print("Source mixing proportions")
-  print(Proportions_est[[it]])
-  
-  
+# Define sources and sinks
 
-}
+sources <- as.matrix(rarefy(otus[train.ix,], COVERAGE))
+sinks <- as.matrix(rarefy(t(as.matrix(otus[test.ix,])), COVERAGE))
 
 
-Proportions_est_data = matrix(unlist(Proportions_est), ncol = (num_sources+1), byrow = T)
-colnames(Proportions_est_data) = c("Baby_B", "Baby_4M", "Baby_M", "unknown")
+print(paste("Number of OTUs in the sink sample = ",length(which(sinks > 0))))
+print(paste("Seq depth in the sources and sink samples = ",COVERAGE))
+print(paste("The sink is:", envs[test.ix]))
+
+# Estimate source proportions for each sink
+
+FEAST_output<-FEAST(source=sources, sinks = t(sinks), env = envs[train.ix], em_itr = EM_iterations, COVERAGE = COVERAGE)
+Proportions_est <- FEAST_output$data_prop[,1]
+names(Proportions_est) <- c(as.character(envs[train.ix]), "unknown")
+
+print("Source mixing proportions")
+Proportions_est
 
