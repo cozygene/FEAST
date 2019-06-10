7a9341b624f219f7f0947f3267013a1ae9aed62d
Liat Shenhav <liatshenhav@MacBook-Pro-3.local>
2019-06-10 07:23:40 -0700

    Remove duplicated directory

--- code/Main_Baby_time_series.R
+++
@@ -1,7 +0,0 @@
-Analysis_flag = 1 #set to 1 to run the analysis, 0 otherwise
-
-#Analysis. analysis running-time IS 0.16 hours 
-
-#Analysis---
-if(Analysis_flag == 1)
-  source("Run_Baby_TS_analysis.R")
--- code/Main_allo_HSCT-analysis.R
+++
@@ -1,8 +0,0 @@
-Analysis_flag = 1 #set to 1 to run the analysis, 0 otherwise
-
-# Analysis running-time is approx. 0.5 hours 
-
-#Analysis---
-if(Analysis_flag == 1)
-  source("Run_allo_HSCT_analysis.R")
-
