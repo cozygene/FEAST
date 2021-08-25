954865857b4de8c9c27d1209802882a58828e345
liashenhav <35780866+liashenhav@users.noreply.github.com>
2021-08-25 19:17:50 -0400

    Update FEAST.R

--- R/FEAST.R
+++ R/FEAST.R
@@ -146,6 +146,6 @@ FEAST <- function(C, metadata, EM_iterations = 1000, COVERAGE = NULL ,different_
 
   setwd(dir_path)
   write.table(proportions_mat, file = paste0(outfile,"_source_contributions_matrix.txt"), sep = "\t")
-  return(proportions_mat, FEAST_output)
+  return(list = c(proportions_mat, FEAST_output))
 
 }
